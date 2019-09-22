c5d227fac7f0eec52f6aee084d3dddc8af7edb04
fighterultimate <carlospsosame@gmail.com>
2019-09-21 22:03:55 -0400

    Testing

---
+++ models/burger.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, DataTypes) {
+    var Post = sequelize.define("burgers", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      }
+    })};
---
+++ public/assets/js/cats.js
@@ -0,0 +1,86 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-sleep").on("click", function(event) {
+    var id = $(this).data("id");
+    var newSleep = $(this).data("newsleep");
+
+    var newSleepState = {
+      sleepy: newSleep
+    };
+
+    
+    // Send the PUT request.
+    $.ajax("/api/cats/" + id, {
+      type: "PUT",
+      data: newSleepState
+    }).then(
+      function() {
+        console.log("changed sleep to", newSleep);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+
+
+
+  $("#button").on("click", function(event){
+
+    var data = $("#exampleFormControlTextarea1").val().trim();
+    alert(data);
+    $.ajax("/api/burger/", {
+      type: "POST",
+      data: data
+    }).then(
+      function() {
+        console.log("this is it", data);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+    
+    
+    
+        });
+
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newCat = {
+      name: $("#ca").val().trim(),
+      sleepy: $("[name=sleepy]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/cats", {
+      type: "POST",
+      data: newCat
+    }).then(
+      function() {
+        console.log("created new cat");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-cat").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/cats/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted cat", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
